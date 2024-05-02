4c2a902e76daee9efd8aa3caecd004d069119a2d
AliBaBas02 <kostasstilianou@gmail.com>
2024-05-02 17:25:01 +0300

    Updated About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -8,14 +8,15 @@ const aboutQuests = ref([
   {question: "What is PokeInfo?", answer: "A web app on which you can find general information about all pokemons."},
   {question: "What information PokeInfo provides about pokemons?", answer: "PokeInfo provides characteristics such as weight, height,type , cries, abilities. It also provides evolutions, stats (such as hp, attack, def etc) and finally weaknesses."},
   {question: "Why i created PokeInfo?", answer: "I created  this website because i wanted to further my skills and knowledge as a web, dev and also because i love the world of pokemon."},
+  {question: "Why some of the data is missing?", answer: "If some data is missing (i.e evolutions) is probably because of the PokeAPI i am currently using to fetch the data."},
 ])
 
 const i = ref(0)
 const rem = ref(0)
 
  setInterval(() => {
-    (i.value === 2) ? i.value=0 : i.value+=1;
-    (i.value <= 3) ? (i.value === 3) ? rem.value=0 : rem.value=i.value : null;
+    (i.value === 3) ? i.value=0 : i.value+=1;
+    (i.value <= 4) ? (i.value === 4) ? rem.value=0 : rem.value=i.value : null;
   },5000);
 </script>
 
@@ -34,8 +35,8 @@ const rem = ref(0)
           <hgroup @mouseover="i=5" @mouseout="i=rem" v-for="(about,index) in aboutQuests" :key="index"
                   :class="[(i===index) ? `text-sky-500 scale-105 transition-all duration-1000 ease-in` : `transition-all duration-1000 ease-out`]"
                   class=" break-inside-auto hover:text-sky-500 hover:scale-105 hyphens-auto text-neutral-800 self-start text-start max-lg:self-center max-lg:text-center">
-            <h1 class="text-xl font-bold"><Icon class="inline size-4" icon="bi:diamond-fill"/> {{about.question}}</h1>
-            <p class="font-semibold">{{about.answer}}</p>
+            <h1 class="max-sm:text-lg text-xl font-bold"><Icon class="inline size-4" icon="bi:diamond-fill"/> {{about.question}}</h1>
+            <p class="max-sm:text-sm font-semibold">{{about.answer}}</p>
           </hgroup>
         </TransitionGroup>
 
